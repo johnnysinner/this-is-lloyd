31c39daf1bb556a465bc18b45d13ea280ffaf033
COMPUTER <COMPUTER@DESKTOP-UUK4VR6>
2020-09-21 18:16:16 +0800

    first commit

---
+++ Sample Project/src/com/svi/main/MainClass.java
@@ -0,0 +1,29 @@
+package com.svi.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test");
+
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		testProgram();
+	}
+
+	public static void testProgram() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("test");
+			System.out.println("anothert");
+			for (int x = 0; x < 10; x++) {
+				System.out.println("test");
+				System.out.println("anothert");
+
+			}
+		}
+	}
+
+}
